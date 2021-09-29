78181f407cda2a19b6f93af43274d2f0e1c702d5
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-09-29 15:12:42 -0300

    Implementação - criação da model Task e do object TaskDataSource

---
+++ app/src/main/java/com/zannardyapps/mylist/datasource/TaskDataSource.kt
@@ -0,0 +1,7 @@
+package com.zannardyapps.mylist.datasource
+
+import com.zannardyapps.mylist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+}
---
+++ app/src/main/java/com/zannardyapps/mylist/model/Task.kt
@@ -0,0 +1,8 @@
+package com.zannardyapps.mylist.model
+
+data class Task(
+    val title: String,
+    val description: String,
+    val date: String,
+    val time: String
+)
